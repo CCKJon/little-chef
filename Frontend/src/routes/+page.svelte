d81c3b1bb4a16e8624cc201f9082b31495faa568
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-12-01 23:47:46 -0600

    updates

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -124,7 +124,7 @@
 		>
 			Welcome to Gusteau’s, bon apetit
 			<div class="w-full text-orange-400 tracking-widest text-center text-xl">
-				Starring our little chef
+				Starring: little chef
 			</div>
 		</h1>
 		<div
